4e3fe1f6d690d7a11c146216a5ecb668e7e82048
Shobhit Sirohi <shobhitsirohi.dev@gmail.com>
2022-03-02 10:15:57 +0530

    final product

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 console.log("🍌");
+
 var btnTranslate = document.querySelector("#btn-translate");
 var input = document.querySelector("#input");
 var output = document.querySelector("#output");
+var url =
+  "https://api.funtranslations.com/translate/minion.json";
 
-function clickHandler() {
-  output.innerText = "gg";
+function constructUrl(text) {
+  return url + "?text=" + text;
 }
-
+function errorhandler(error) {
+  console.log(error);
+  alert("SERVER DOWN, TRY AGAIN LATER.");
+}
+function clickHandler() {
+  var text = input.value;
+  fetch(constructUrl(text))
+    .then((response) => response.json())
+    .then(
+      (data) => (output.value = data.contents.translated)
+    )
+    .catch(errorhandler);
+}
 btnTranslate.addEventListener("click", clickHandler);
